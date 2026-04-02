06d8561051e3c7615e80fd9c5fc710114738d0de
Himali Silwadiya <himalisilwadiya@gmail.com>
2026-04-02 18:34:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,94 @@
+//variable ko kaise banate hai
+// let name="Himali";
+// let age=22;
+// age=25;
+// console.log(name,age);
+// const account=12345;
+// // account=123; not possible
+// console.log(account);
+
+// if(true){
+//     var a=10;
+// }
+// console.log(a)
+
+//String
+// let a ='Himali';
+// console.log(a);
+
+//Boolean
+// let login=true;
+// let f=false;
+// console.log(login,f);
+
+//Undefined
+// let user;
+// console.log(user);
+
+//bigint
+// let n=198374756352416182039n;
+// console.log(n);
+
+//null
+// let weather=null;
+// console.log(weather);
+
+//Symbol
+// const id=Symbol("i");
+// console.log(id);
+
+// let a=[10,237.90,'Himali',true];
+// console.log(a)
+
+// function add(){
+//     console.log("Hyyyy");
+// }
+// add();
+
+//primitive data types are immutable
+// let s="Rohit";
+// s[0]='M';
+// console.log(s);
+
+//non-primitive data types are mutable
+// let arr=[10,20,40 ,50];
+// arr[2]=30;
+// console.log(arr)
+
+// let user={
+//     name:'Rohit',
+//     age:25
+// }
+// user.name='Mohit'
+// let user1=user;
+// user1.name='Sham';
+// console.log(user);
+
+//Numberr
+// let a=19;
+// let b=374.6717865;
+// // b.toFixed(1);
+// // console.log( b);
+// console.log(b.toPrecision(7))
+
+// let a=new Number(20);
+// let b=new Number(20)
+// a=b
+// console.log(a==b);
+// let obj1={
+//     name:"Himali"
+// }
+// let obj2={
+//     name:"Himali"
+// }
+// obj1=obj2
+// console.log(obj1==obj2)
+
+// console.log(Boolean(undefined))
+
+// console.log(Math.floor(Math.random()*10+1)+15);
+
+//String
+const day=18;
+const st=`Strike is coming soon on ${day}`;
+console.log(st);
